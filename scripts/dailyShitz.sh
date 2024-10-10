4234a20fce80d9a3bd94f616ca23e60ce14b8d6c
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2024-10-10 18:48:56 +0530

    daily work vale ko understandable banaya

--- scripts/dailyShitz.sh
+++ scripts/dailyShitz.sh
@@ -1,52 +1,31 @@
 #!/bin/bash
 
-# Open Chrome in workspace 2
+# Chrome in workspace 2
 i3-msg "workspace 2"
 sleep 0.3
 i3-msg "exec google-chrome-stable"
-sleep 0.7 # Shorter delay to allow Chrome to launch
+sleep 0.7
 
-# Open Spotify in workspace 3
+# Spotify in workspace 3
 i3-msg "workspace 3"
 sleep 0.3
 i3-msg "exec spotify"
-sleep 0.7 # Shorter delay to allow Spotify to launch
+sleep 0.7
 
-# Return to workspace 1 and open Alacritty terminals
+# Back to workspace 1 and open Alacritty
 i3-msg "workspace 1"
 
-# Open the first Alacritty terminal with a new tmux session (no name)
+# First terminal
 sleep 0.3
 i3-msg "exec alacritty -e tmux"
 
-# Open the second Alacritty terminal with a new tmux session named "Scratchpad"
-sleep 0.7 # Shorter delay to ensure the first terminal is launched
+# Second terminal as a scratchpad
+sleep 0.7
 i3-msg "exec alacritty -e tmux new-session -s Scratchpad"
-
-# Wait a bit longer to ensure both terminals are ready
 sleep 0.5
 
-# Get the window ID of the last opened Alacritty window (second terminal)
+# Get the window ID of the second terminal
 SECOND_TERM_ID=$(xdotool search --onlyvisible --class Alacritty | tail -n 1)
 
-# Move the second terminal to the scratchpad using its window ID
+# Window ID se second terminal ko scratchpad me convert
 i3-msg "[id=$SECOND_TERM_ID] move scratchpad"
-
-##!/bin/bash
-#
-## Open terminal in workspace 1
-#i3-msg "workspace 1"
-#sleep 0.3
-#i3-msg "exec alacritty"
-#sleep 0.7 # Shorter delay to ensure Alacritty launches
-#
-## Open Chrome in workspace 2
-#i3-msg "workspace 2"
-#sleep 0.3
-#i3-msg "exec google-chrome-stable"
-#sleep 0.7 # Shorter delay to allow Chrome to launch
-#
-## Open Spotify in workspace 3
-#i3-msg "workspace 3"
-#sleep 0.3
-#i3-msg "exec spotify"
