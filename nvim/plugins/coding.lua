dbe248e4b5bfd767a2187f3deb8c8d7aa25aa000
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2024-11-22 23:48:11 +0530

    Kuch toh kra hai

--- nvim/plugins/coding.lua
+++ nvim/plugins/coding.lua
@@ -6,58 +6,58 @@ return {
     config = true,
   },
   { "wakatime/vim-wakatime", lazy = false },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    keys = {
-      {
-        "<leader>ucc",
-        function()
-          if vim.g.copilot_status == nil then
-            vim.g.copilot_status = "running"
-          end
-          if vim.g.copilot_status == "running" then
-            vim.g.copilot_status = "stopped"
-            vim.cmd("Copilot disable")
-          else
-            vim.g.copilot_status = "running"
-            vim.cmd("Copilot enable")
-          end
-        end,
-        desc = "Toggle Copilot",
-      },
-    },
-    opts = {
-      filetypes = {
-        yaml = false,
-        markdown = true,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-      suggestion = {
-        enable = true,
-        auto_trigger = true,
-      },
-    },
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    event = "VeryLazy",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   keys = {
+  --     {
+  --       "<leader>ucc",
+  --       function()
+  --         if vim.g.copilot_status == nil then
+  --           vim.g.copilot_status = "running"
+  --         end
+  --         if vim.g.copilot_status == "running" then
+  --           vim.g.copilot_status = "stopped"
+  --           vim.cmd("Copilot disable")
+  --         else
+  --           vim.g.copilot_status = "running"
+  --           vim.cmd("Copilot enable")
+  --         end
+  --       end,
+  --       desc = "Toggle Copilot",
+  --     },
+  --   },
+  --   opts = {
+  --     filetypes = {
+  --       yaml = false,
+  --       markdown = true,
+  --       help = false,
+  --       gitcommit = false,
+  --       gitrebase = false,
+  --       hgcommit = false,
+  --       svn = false,
+  --       cvs = false,
+  --       ["."] = false,
+  --     },
+  --     suggestion = {
+  --       enable = true,
+  --       auto_trigger = true,
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   event = "VeryLazy",
+  --   branch = "canary",
+  --   dependencies = {
+  --     { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+  --     { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+  --   },
+  --   opts = {
+  --     debug = true, -- Enable debugging
+  --     -- See Configuration section for rest
+  --   },
+  --   -- See Commands section for default commands if you want to lazy load on them
+  -- },
 }
--- nvim/plugins/ui.lua
+++ nvim/plugins/ui.lua
@@ -130,7 +130,7 @@ return {
 ]]
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
+      opts.header = vim.split(logo, "\n")
     end,
   },
 }
