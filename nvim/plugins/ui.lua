1da963499540cf55476849252f9d93120e4f8e64
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-02-22 23:41:30 +0530

    shitz added for nvim

--- nvim/plugins/ui.lua
+++ nvim/plugins/ui.lua
@@ -449,51 +449,23 @@ ___  ---  ___  ---  ___
       })
     end,
   },
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   dependencies = { "nvim-tree/nvim-web-devicons" },
-  --   config = function()
-  --     require("lualine").setup({
-  --       options = {
-  --         icons_enabled = true,
-  --         theme = "auto",
-  --         component_separators = { left = "", right = "" },
-  --         section_separators = { left = "", right = "" },
-  --         disabled_filetypes = {
-  --           statusline = {},
-  --           winbar = {},
-  --         },
-  --         ignore_focus = {},
-  --         always_divide_middle = true,
-  --         always_show_tabline = true,
-  --         globalstatus = false,
-  --         refresh = {
-  --           statusline = 100,
-  --           tabline = 100,
-  --           winbar = 100,
-  --         },
-  --       },
-  --       sections = {
-  --         lualine_a = { "mode" },
-  --         lualine_b = { "branch", "diff", "diagnostics" },
-  --         lualine_c = { "filepath" },
-  --         lualine_x = { "encoding", "fileformat", "filetype" },
-  --         lualine_y = { "progress", "location" },
-  --         lualine_z = {},
-  --       },
-  --       inactive_sections = {
-  --         lualine_a = {},
-  --         lualine_b = {},
-  --         lualine_c = { "filename" },
-  --         lualine_x = { "location" },
-  --         lualine_y = {},
-  --         lualine_z = {},
-  --       },
-  --       tabline = {},
-  --       winbar = {},
-  --       inactive_winbar = {},
-  --       extensions = {},
-  --     })
-  --   end,
-  -- },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      cursor_color = "#ff8800",
+      stiffness = 0.3,
+      trailing_stiffness = 0.1,
+      trailing_exponent = 5,
+      hide_target_hack = true,
+      gamma = 1,
+    },
+  },
+  {
+    "Eandrju/cellular-automaton.nvim",
+    cmd = "CellularAutomaton",
+  },
+  {
+    "alec-gibson/nvim-tetris",
+    cmd = "Tetris",
+  },
 }
