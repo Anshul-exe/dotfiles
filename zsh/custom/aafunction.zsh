a7030d0eaa3903138c42f076a3e345ceddf44f05
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-02-28 01:15:11 +0530

    ahaaa

--- zsh/custom/aafunction.zsh
+++ zsh/custom/aafunction.zsh
@@ -104,3 +104,25 @@ function y() {
 	fi
 	rm -f -- "$tmp"
 }
+
+## cd spf to last viewing dir
+spf() {
+    os=$(uname -s)
+
+    # Linux
+    if [[ "$os" == "Linux" ]]; then
+        export SPF_LAST_DIR="${XDG_STATE_HOME:-$HOME/.local/state}/superfile/lastdir"
+    fi
+
+    # macOS
+    if [[ "$os" == "Darwin" ]]; then
+        export SPF_LAST_DIR="$HOME/Library/Application Support/superfile/lastdir"
+    fi
+
+    command spf "$@"
+
+    [ ! -f "$SPF_LAST_DIR" ] || {
+        . "$SPF_LAST_DIR"
+        rm -f -- "$SPF_LAST_DIR" > /dev/null
+    }
+}
