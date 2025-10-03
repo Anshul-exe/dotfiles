33dca2b76b0132cba5edb8d11f088986956c76b3
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-10-03 05:33:22 +0530

    datesheet and mess schedule added in wallpaper

--- scripts/feh_switcher.sh
+++ scripts/feh_switcher.sh
@@ -36,6 +36,8 @@ wallpapers=(
   "Hacker|post-apocalyptic_hacker.png"
   "Leaves|purple_leaves.png"
   "Mountains|purpleMountain.jpg"
+  "datesheet|datesheet.jpeg"
+  "mess|mess.jpeg"
 )
 
 # Generate rofi menu options
