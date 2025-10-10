603b7ddca153096b4c43002ea9d860de71f38169
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-10-11 01:49:41 +0530

    lspsaga added

--- nvim/plugins/coding.lua
+++ nvim/plugins/coding.lua
@@ -267,6 +267,17 @@ return {
       }
     end,
   },
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "LspAttach",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("lspsaga").setup({})
+    end,
+  },
 }
 
 -- make table
