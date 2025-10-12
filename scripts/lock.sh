609086935c752b7de8f7878fdedbfd1d33466d96
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-10-13 01:40:07 +0530

    lock script

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# Fix environment for SSH → local graphical session
+export DISPLAY=:0
+export XAUTHORITY=/home/mir/.Xauthority
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+
 # Reduce screen brightness to 0
-brightnessctl set 0
+sudo brightnessctl set 0
 
 # Turn off Bluetooth
 bluetoothctl power off
