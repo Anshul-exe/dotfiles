681cc1987e84915b8c72924c35637576f3889b8a
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-10-21 01:04:17 +0530

    Authentication removed from lock script

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -6,7 +6,7 @@ export XAUTHORITY=/home/mir/.Xauthority
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
 
 # Reduce screen brightness to 0
-sudo brightnessctl set 0
+brightnessctl set 0
 
 # Turn off Bluetooth
 bluetoothctl power off
