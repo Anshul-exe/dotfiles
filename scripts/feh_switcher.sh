b45a57e5b443c236968206fafd20f4fe2401f30b
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-10-22 01:03:37 +0530

    image added in wallpaper switcher

--- scripts/feh_switcher.sh
+++ scripts/feh_switcher.sh
@@ -36,6 +36,7 @@ wallpapers=(
   "Hacker|post-apocalyptic_hacker.png"
   "Leaves|purple_leaves.png"
   "Mountains|purpleMountain.jpg"
+  "Pixel Purple|purplePixel.png"
   "datesheet|datesheet.jpeg"
   "mess|mess.jpeg"
 )
