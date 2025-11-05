4b390dcbd19386921eb8ca4d2c7e9da8ecdf5351
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-11-05 21:13:08 +0530

    alias updated

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -123,8 +123,9 @@ alias f1='gcalendar --calendar="Formula 1" --no-of-days=30'
 alias server='ip=$(ip -4 addr show scope global | grep -oP "(?<=inet\s)\d+(\.\d+){3}"); echo "🚀 Serving at: http://$ip:6789"; python3 -m http.server 6789 --bind 0.0.0.0' # run kro jaha se files share krni hai and access through http://<this-system's-ip>:6789 from another system on the same network
 alias lsblk="lsblk | bat -l conf -p"
 alias connected='bluetoothctl devices Connected | awk '\''{$1=$2=""; print substr($0,3)}'\'''
+alias nyancat="nyancat -s"
 
 # phone controls
 alias jj="amixer set Master 5%-"
 alias kk="amixer set Master 5%+"
-alias plause="playerctl play-pause"
+alias plause="playerctl -p spotify play-pause"
